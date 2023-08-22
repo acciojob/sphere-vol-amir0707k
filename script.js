55f88740629d1cfd9f7545d0013648eb4043810e
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-08-22 14:27:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
    function volume_sphere(){
-               const radius = parseFloat(document.getElementById("radius").value);
+        const radius = parseFloat(document.getElementById("radius").value);
         event.preventDefault();
         console.log(radius);
         console.log(typeof radius)
         const volume = (4/3)*Math.PI*Math.pow(radius,3);
-        document.getElementById("volume").value = volume;
+        document.getElementById("volume").value = volume.toFixed(4);
     }
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
